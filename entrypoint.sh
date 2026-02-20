98dd0f6e647da1d41255fa5e50429946d0089d50
mrbeenopro <mrbeenopro@gmail.com>
2026-02-20 12:08:31 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,16 @@ cd /home/container
 
 VNC_PORT=5901
 
-echo "[+] QEMU start display on :1 -> port 5901"
-echo "[+] noVNC web :$SERVER_PORT"
+echo "[+] QEMU VNC :1 → 5901"
+echo "[+] noVNC Web :$SERVER_PORT"
 
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-
-# Run VM
 eval ${MODIFIED_STARTUP} &
 
 sleep 2
 
 cd /opt/novnc
 
-# Serve web
-python3 -m http.server ${SERVER_PORT} &
-
-sleep 1
-
-# Bridge VNC
-./utils/novnc_proxy --vnc localhost:${VNC_PORT} --listen ${SERVER_PORT}
+./utils/novnc_proxy \
+  --vnc localhost:${VNC_PORT} \
+  --listen 0.0.0.0:${SERVER_PORT}
