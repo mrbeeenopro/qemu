c8b8a35fba80cd1fb0d9e0497856cad6b2c96c32
mrbeenopro <mrbeenopro@gmail.com>
2026-04-30 14:40:06 +0700

    Enhance entrypoint.sh with color-coded messages
    Added ANSI color codes for better output visibility and improved startup messages.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# --- 1. Define ANSI Color Codes ---
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+
+echo -e "${GREEN}[+] Initializing environment..."
+
 cd /home/container
 
 # Fix QEMU temp write
@@ -7,20 +18,24 @@ mkdir -p $TMPDIR
 
 VNC_PORT=5901
 
+echo -e "${GREEN}[+] Network Configuration:${NC}"
+echo -e " ↳ QEMU VNC : ${YELLOW}1 → 5901${NC}"
+echo -e " ↳ noVNC Web: ${YELLOW}$SERVER_PORT${NC}"
+
+echo -e "${GREEN}[+] Starting noVNC in the background...${NC}"
 cd /opt/novnc
 
 ./utils/websockify/run \
   --web /opt/novnc \
   0.0.0.0:${SERVER_PORT} \
-  localhost:${VNC_PORT}
-echo "[+] QEMU VNC :1 → 5901"
-echo "[+] noVNC Web :$SERVER_PORT"
+  localhost:${VNC_PORT} > /dev/null 2>&1 &
+
 
 sleep 2
-
+-
 cd /home/container
+echo -e "${GREEN}[+] Entering QEMU Console. You can type your commands now!${NC}"
 
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 eval exec ${MODIFIED_STARTUP}
-
