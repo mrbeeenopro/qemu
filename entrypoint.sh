8f6e2a9964470d3967d571324cd2dfbb0c52f358
mrbeenopro <mrbeenopro@gmail.com>
2026-04-30 08:26:49 +0700

    Refactor entrypoint.sh to improve startup sequence
    Moved startup commands to execute after setting up noVNC.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,20 @@ mkdir -p $TMPDIR
 
 VNC_PORT=5901
 
-echo "[+] QEMU VNC :1 → 5901"
-echo "[+] noVNC Web :$SERVER_PORT"
-
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-eval ${MODIFIED_STARTUP} &
-
-sleep 2
-
 cd /opt/novnc
 
 ./utils/websockify/run \
   --web /opt/novnc \
   0.0.0.0:${SERVER_PORT} \
   localhost:${VNC_PORT}
+echo "[+] QEMU VNC :1 → 5901"
+echo "[+] noVNC Web :$SERVER_PORT"
+
+sleep 2
+
+cd /home/container
+
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+
+eval exec ${MODIFIED_STARTUP}
+
